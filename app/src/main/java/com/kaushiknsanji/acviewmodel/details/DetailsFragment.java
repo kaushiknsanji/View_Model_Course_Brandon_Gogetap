3b4bd5c507a1f4346078920bcbd52350fafbae28
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2019-03-27 15:39:10 +0530

    feat: Add Fragment for Details

---
+++ app/src/main/java/com/kaushiknsanji/acviewmodel/details/DetailsFragment.java
@@ -0,0 +1,98 @@
+package com.kaushiknsanji.acviewmodel.details;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kaushiknsanji.acviewmodel.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+public class DetailsFragment extends Fragment {
+
+    @BindView(R.id.tv_repo_name)
+    TextView mTextViewRepoName;
+
+    @BindView(R.id.tv_repo_description)
+    TextView mTextViewRepoDescription;
+
+    @BindView(R.id.tv_forks)
+    TextView mTextViewForks;
+
+    @BindView(R.id.tv_stars)
+    TextView mTextViewStars;
+
+    private Unbinder mUnbinder;
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+     * <p>
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate
+     *                           any views in the fragment,
+     * @param container          If non-null, this is the parent view that the fragment's
+     *                           UI should be attached to.  The fragment should not add the view itself,
+     *                           but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.screen_details, container, false);
+        mUnbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    /**
+     * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+     * has returned, but before any saved state has been restored in to the view.
+     * This gives subclasses a chance to initialize themselves once
+     * they know their view hierarchy has been completely created.  The fragment's
+     * view hierarchy is not however attached to its parent at this point.
+     *
+     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        displayRepo();
+    }
+
+    private void displayRepo() {
+
+    }
+
+    /**
+     * Called when the view previously created by {@link #onCreateView} has
+     * been detached from the fragment.  The next time the fragment needs
+     * to be displayed, a new view will be created.  This is called
+     * after {@link #onStop()} and before {@link #onDestroy()}.  It is called
+     * <em>regardless</em> of whether {@link #onCreateView} returned a
+     * non-null view.  Internally it is called after the view's state has
+     * been saved but before it has been removed from its parent.
+     */
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mUnbinder != null) {
+            //Unbinding the Views when the Fragment View is detached, to prevent leaks
+            mUnbinder.unbind();
+            mUnbinder = null;
+        }
+    }
+}
